d1a7ff135997aeeeca32514162b23292c17f7c61
Ray Andrew <raydreww@gmail.com>
2026-09-23 14:48:51 -0700

    refactor: probe discovery and generation move to datacrumbs-utils

--- src/datacrumbs/composable/process/server.cpp
+++
@@ -1,7 +0,0 @@
-#include <datacrumbs/composable/bpf/datacrumbs.skel.h>
-
-#include <datacrumbs/server/process/server.impl.cpp>
-
-int main(int argc, char** argv) {
-  return main_call(argc, argv);
-}
